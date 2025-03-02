9d5d5b4c54bd334706293d81233e3d5bfc76af90
Ayaismail2022 <120684040+Ayaismail2022@users.noreply.github.com>
2025-03-02 19:23:31 +0200

    Add files via upload

---
+++ task_day2.py
@@ -0,0 +1,12 @@
+#---------------------
+# First task in Camp
+# produce my self
+#---------------------
+name = "Aya"
+age = "22"
+country = "Egypt"
+print("\"Name: " + name +"\"" + "\n\"Age: " + age + "\"" + "\n\"Country: " + country + "\"")
+print("\"Hello, My name is " + name + "I'm " + age + " years old and I live in "+ country +" \" " )
+print(type(name))
+print(type(age))
+print(type(country))
